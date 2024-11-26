7bf89dab050285038ac46920009eebe75135d136
ramaputra1 <166339284+ramaputra1@users.noreply.github.com>
2024-11-25 19:18:17 -1000

    all in

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ const navbar = document.querySelector(".navbar");
 document.querySelector("#nav-extra").onclick = () => {
   navbar.classList.toggle("expand");
 };
+
+//Toogle
+const navbar2 = document.querySelector(".nav-main");
+//Click
+document.querySelector("#content-btn-extra").onclick = () => {
+  navbar2.classList.toggle("expand2");
+};
